9b00a91062a7bf2e00017e149cd3074adb12c1d6
Bolun Zhang <rzhangbolun@gmail.com>
2022-08-29 02:02:35 -0400

    draw Peak logo

--- creating-and-combining-views/Landmarks/Landmarks/Views/Badges/BadgeSymbol.swift
+++ creating-and-combining-views/Landmarks/Landmarks/Views/Badges/BadgeSymbol.swift
@@ -8,9 +8,41 @@
 import SwiftUI
 
 struct BadgeSymbol: View {
+    static let symbolColor = Color(
+        red: 79.0 / 255,
+        green: 79.0 / 255,
+        blue: 191.0 / 255)
+    
+    @State private var width : CGFloat = 0
+    private var height : CGFloat { width * 0.85 }
+    private var spacing : CGFloat { width * 0.030 }
+    private var middle : CGFloat { width * 0.5 }
+    private var topWidth : CGFloat { width * 0.226 }
+    private var topHeight : CGFloat { height * 0.488 }
+
     var body: some View {
         GeometryReader { geometry in
-
+            
+            Path { path in
+                width = min(geometry.size.width, geometry.size.height)
+                path.addLines([
+                    CGPoint(x: middle, y: spacing),
+                    CGPoint(x: middle - topWidth, y: topHeight - spacing),
+                    CGPoint(x: middle, y: topHeight / 2 + spacing),
+                    CGPoint(x: middle + topWidth, y: topHeight - spacing),
+                    CGPoint(x: middle, y: spacing)
+                ])
+                path.move(to: CGPoint(x: middle, y: topHeight / 2 + spacing * 3))
+                path.addLines([
+                    CGPoint(x: middle - topWidth, y: topHeight + spacing),
+                    CGPoint(x: spacing, y: height - spacing),
+                    CGPoint(x: width - spacing, y: height - spacing),
+                    CGPoint(x: middle + topWidth, y: topHeight + spacing),
+                    CGPoint(x: middle, y: topHeight / 2 + spacing * 3)
+                ])
+                
+            }
+            .fill(Self.symbolColor)
         }
     }
 }
