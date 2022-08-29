88c8624186327bdc6a82147156afb7e869fda6fb
Bolun Zhang <rzhangbolun@gmail.com>
2022-08-29 01:30:42 -0400

    download appIcon resource

---
+++ creating-and-combining-views/Landmarks/Landmarks/Views/Badges/BadgeSymbol.swift
@@ -0,0 +1,22 @@
+//
+//  BadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Bolun Zhang on 8/28/22.
+//
+
+import SwiftUI
+
+struct BadgeSymbol: View {
+    var body: some View {
+        GeometryReader { geometry in
+
+        }
+    }
+}
+
+struct BadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        BadgeSymbol()
+    }
+}
