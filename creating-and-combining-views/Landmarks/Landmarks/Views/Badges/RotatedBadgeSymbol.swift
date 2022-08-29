8bf1c3b7af0feb302581de9b89e4e270eafc0cbb
Bolun Zhang <rzhangbolun@gmail.com>
2022-08-29 02:31:58 -0400

    BadgeSymbol not working version
    not shown in Badge

---
+++ creating-and-combining-views/Landmarks/Landmarks/Views/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,23 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Bolun Zhang on 8/29/22.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    var angle : Angle
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
