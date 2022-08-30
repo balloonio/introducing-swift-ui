7c948d3880f3305d29e334b1e1f57f4309f97cb6
Bolun Zhang <rzhangbolun@gmail.com>
2022-08-30 00:46:11 -0400

    fix bug on observations naming and distanceText

--- creating-and-combining-views/Landmarks/Landmarks/Model/Hike.swift
+++ creating-and-combining-views/Landmarks/Landmarks/Model/Hike.swift
@@ -12,12 +12,18 @@ struct Hike : Codable, Identifiable, Hashable {
     var id : Int
     var distance: Double
     var difficulty: Int
-    var observation : [Observation]
+    var observations : [Observation]
+    
+    static var formatter = LengthFormatter()
+    
+    var distanceText : String {
+        Hike.formatter.string(fromValue: distance, unit: .kilometer)
+    }
     
     struct Observation : Codable, Hashable {
-        var elevation: [Double]
-        var pace: [Double]
-        var heartRate: [Double]
+        var elevation: Range<Double>
+        var pace: Range<Double>
+        var heartRate: Range<Double>
         var distanceFromStart: Double
     }
 }
